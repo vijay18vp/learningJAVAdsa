fd410e37437c72cc05410093c97eec339cdcb684
vijay18vp <voidprotocol18@gmail.com>
2025-08-15 11:59:00 +0530

    Added Comments

--- 1.IntroForJava/Basic.java
+++ 1.IntroForJava/Basic.java
@@ -1,5 +1,10 @@
+// This is my first code in JAVA 
+
 public class Basic {
+
+    // used PSVM shortcut to get this template
     public static void main(String[] args) {
+        // used sOut shortcut  to get this line 
         System.out.println("Hello World!");
     }
 }
