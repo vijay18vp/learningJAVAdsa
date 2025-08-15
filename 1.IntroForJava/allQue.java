f3f1011cbc357ad3fb6ba8e182292608fed24180
vijay18vp <voidprotocol18@gmail.com>
2025-08-15 21:13:56 +0530

    Added one new question

--- 1.IntroForJava/allQue.java
+++ 1.IntroForJava/allQue.java
@@ -73,32 +73,55 @@
 
 //Que4 - Take in two numbers and an operator (+, -, * /) and calculate the value. (Use if conditions)
 
-public class allQue {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter you first number: ");
-        double num1 = sc.nextDouble();
-        System.out.print("Enter your second number: ");
-        double num2 = sc.nextDouble();
-        System.out.println("Enter the Operation: ");
-        System.out.print("Choose operator:" + "1.+ " + "2.- " + "3.* " + "4./");
-        int choose = sc.nextInt();
+// public class allQue {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter you first number: ");
+//         double num1 = sc.nextDouble();
+//         System.out.print("Enter your second number: ");
+//         double num2 = sc.nextDouble();
+//         System.out.println("Enter the Operation: ");
+//         System.out.print("Choose operator:" + "1.+ " + "2.- " + "3.* " + "4./");
+//         int choose = sc.nextInt();
 
-        if(choose == 1){
-            double Sum = num1 + num2 ;
-            System.out.print("The Sum of Two number is " + Sum);
-        }else if(choose == 2){
-            double Difference = num1 - num2 ;
-            System.out.println("The Difference between the numbers is " + Difference);
-        }else if(choose == 3){
-            double Product = num1*num2;
-            System.out.println("The Product of two number is " + Product);
-        }else{
-            double Division = num1/num2;
-            System.out.println("The Division of two number is " + Division);
+//         if(choose == 1){
+//             double Sum = num1 + num2 ;
+//             System.out.print("The Sum of Two number is " + Sum);
+//         }else if(choose == 2){
+//             double Difference = num1 - num2 ;
+//             System.out.println("The Difference between the numbers is " + Difference);
+//         }else if(choose == 3){
+//             double Product = num1*num2;
+//             System.out.println("The Product of two number is " + Product);
+//         }else{
+//             double Division = num1/num2;
+//             System.out.println("The Division of two number is " + Division);
+//         }
+
+//         sc.close();
+
+//     }
+// }
+
+
+
+//Que5 - Take 2 numbers as input and print the largest number.
+public class allQue {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First number: ");
+        double num1 = sc.nextDouble();
+          System.out.print("Enter the Second number: ");
+        double num2 = sc.nextDouble();
+
+        if(num1 > num2){
+            System.out.println("num1 is greater than num2 ");
+        }else if(num1 < num2 ){
+            System.out.println("num1 is smaller than num2 ");
+        }else {
+           System.out.println("num1 is equal to num2 ");    
         }
 
         sc.close();
-
-    }
+      }
 }
