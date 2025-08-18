44f28244db97ded913decef889d60a81d1e81a9b
vijay18vp <voidprotocol18@gmail.com>
2025-08-18 22:00:27 +0530

    Practiced Questions

--- 2.ConditionalS/Questions.java
+++ 2.ConditionalS/Questions.java
@@ -1,14 +1,32 @@
  import java.util.Scanner;
-public class Questions {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Radius of the circle: ");
-        double radius = sc.nextDouble();
+// public class Questions {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the Radius of the circle: ");
+//         double radius = sc.nextDouble();
 
-        // now use formula for calculating area of circle 
-        double areaOfCircle = 3.14*(radius*radius);
-        System.out.println("The Area of the circle is " + areaOfCircle );
-sc.close();
+//         // now use formula for calculating area of circle 
+//         double areaOfCircle = 3.14*(radius*radius);
+//         System.out.println("The Area of the circle is " + areaOfCircle );
+// sc.close();
         
-    }
+//     }
+// }
+
+
+// Que2 - Area Of Triangle 
+public class Questions {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the hieght of The Triangle : ");
+    double hieght  = sc.nextDouble();
+    System.out.print("Enter the base of The Triangle : ");
+    double base = sc.nextDouble();
+
+    // formula for Triangle Area 
+   double area = 0.5 *(hieght*base);
+   System.out.println("The area Of the Triangle is " + area );
+
+   }
 }
