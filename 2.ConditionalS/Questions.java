551fda1e34d746474eb136a88d026324aa9c1162
vijay18vp <voidprotocol18@gmail.com>
2025-08-18 22:23:38 +0530

    Questions

--- 2.ConditionalS/Questions.java
+++ 2.ConditionalS/Questions.java
@@ -15,18 +15,36 @@
 
 
 // Que2 - Area Of Triangle 
+// public class Questions {
+//    public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.print("Enter the hieght of The Triangle : ");
+//     double hieght  = sc.nextDouble();
+//     System.out.print("Enter the base of The Triangle : ");
+//     double base = sc.nextDouble();
+
+//     // formula for Triangle Area 
+//    double area = 0.5 *(hieght*base);
+//    System.out.println("The area Of the Triangle is " + area );
+
+//    }
+// }
+
+
+//Que3 - Area Of Rectangle Program
+
 public class Questions {
-   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Height : ");
+        double height = sc.nextDouble();
+        System.out.println("Enter the Width : ");
+        double width = sc.nextDouble();
 
-    System.out.print("Enter the hieght of The Triangle : ");
-    double hieght  = sc.nextDouble();
-    System.out.print("Enter the base of The Triangle : ");
-    double base = sc.nextDouble();
+        double area = width * height ;
 
-    // formula for Triangle Area 
-   double area = 0.5 *(hieght*base);
-   System.out.println("The area Of the Triangle is " + area );
-
-   }
+        System.out.println("The Area Of Reactangle is : " + area );
+        sc.close();
+ }
 }
