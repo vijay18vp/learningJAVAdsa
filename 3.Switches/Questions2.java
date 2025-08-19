7067bfeff58a4463172be680e6c3a1d721e0f86f
vijay18vp <voidprotocol18@gmail.com>
2025-08-19 13:01:06 +0530

    Functions / Methods

--- 3.Switches/Questions2.java
+++ 3.Switches/Questions2.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
 
 // program to write switch 
-public class Questions2 {
-      public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number from 1 - 7 : ");
-        int date = sc.nextInt();
+// public class Questions2 {
+//       public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the number from 1 - 7 : ");
+//         int date = sc.nextInt();
 
-//using traditional switches 
-    switch(date){
-        case 1 : 
-        System.out.println("Monday");
-        break;
-        case 2 :
-        System.out.println("Tuesday");
-        break;
-        case 3 : 
-        System.out.println("Wednesday");
-        break;
-        case 4 : 
-        System.out.println("Thursday");
-        break ; 
-        case 5 : 
-        System.out.println("Friday");
-        break ; 
-        case 6 : 
-        System.out.println("Saturday");
-        break ; 
-        case 7 : 
-        System.out.println("Sunday");
-        break ;
-        default :
-        System.out.println("Please Enter the Valid number ");
-    }
+// //using traditional switches 
+//     switch(date){
+//         case 1 : 
+//         System.out.println("Monday");
+//         break;
+//         case 2 :
+//         System.out.println("Tuesday");
+//         break;
+//         case 3 : 
+//         System.out.println("Wednesday");
+//         break;
+//         case 4 : 
+//         System.out.println("Thursday");
+//         break ; 
+//         case 5 : 
+//         System.out.println("Friday");
+//         break ; 
+//         case 6 : 
+//         System.out.println("Saturday");
+//         break ; 
+//         case 7 : 
+//         System.out.println("Sunday");
+//         break ;
+//         default :
+//         System.out.println("Please Enter the Valid number ");
+//     }
 
-    sc.close();
+//     sc.close();
 
-      }
-}
+//       }
+// }
+
+
+
---
+++ 4.Methods/hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+
+public class hello {
+
+  public static void greet(String name) {
+      System.out.println("Hello " + name + " Welcome to methods Lecture !");
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Your Name : ");
+       String name = sc.nextLine();
+
+       greet(name);
+  }
+}
