d60d45e40f8ea576bca6a077982350c6fcfe4a96
vijay18vp <voidprotocol18@gmail.com>
2025-09-06 19:33:18 +0530

    Change in array by a method learned

---
+++ 4.Methods/changeValue.java
@@ -0,0 +1,12 @@
+// to see whether the change is reflected in the main method or not 
+public class changeValue {
+    public static void main(String[] args) {
+        int[] marks = {97 , 98 , 95};
+        System.out.println("The value of the marks is before change is :" + marks[0]);
+        change(marks);
+    }
+    static void change(int[] marks){
+        marks[0] = 100 ;
+        System.out.println("The value of the marks after change is : " + marks[0]);
+    }
+}
